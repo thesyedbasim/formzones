35887fc200cea8be33203c4b3da7e590f3246321
Syed Basim <social.syedbasim@gmail.com>
2024-07-04 23:44:40 +0530

    fix next/router --> next/navigation

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/lib/firebase/auth';
 
