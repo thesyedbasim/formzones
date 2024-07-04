6f7d1b01239cd3db905e6ab5479d518e87f4c227
Syed Basim <social.syedbasim@gmail.com>
2024-07-04 22:52:05 +0530

    add app layout auth check for redirect

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/lib/firebase/auth';
+
+export default function AppLayout() {
+  const router = useRouter();
+  const [user, isLoading, error] = useAuthState(auth);
+
+  if (isLoading) return <p className="">Loading...</p>;
+
+  if (error) return <p className="">Error occurred!</p>;
+
+  if (!user) return router.push('/auth');
+
+  return 'you are logged in';
+}
